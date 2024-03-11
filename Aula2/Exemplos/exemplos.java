0f4f458751943536cdfc5635c77e84e5b890c9e7
Matheus Eduardo dos Santos <matheusthja1@gmail.com>
2024-03-11 11:21:35 -0300

    Finalizei o README.md do Aula2

--- Aula2/Exemplos/exemplos.java
+++ Aula2/Exemplos/exemplos.java
@@ -1,14 +1,20 @@
 package Aula2.Exemplos;
 
 import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.Vector;
 
 public class exemplos {
     public static void main(String[] args) {
         vectorExample();
         arrayListExample();
+        linkedListExample();
+        hashMapExample();
     }
     private static void vectorExample(){
+        System.out.println("\nExemplo de Vector:");
+
         //Cria um vector que armazena strings, 
         //com o tamanho inicial 10, e que incrementa em 1 
         //cada vez que o limite é excedido
@@ -19,10 +25,13 @@ public class exemplos {
         for (String str : frutas) {
             System.out.println(str);
         }
+        
         //Irá imprimir a capacidade atual do vector(10)
         System.out.println(frutas.capacity());
     }
     private static void arrayListExample(){
+        System.out.println("\nExemplo de ArrayList:");
+
         //Cria um arraylist que armazena strings
         ArrayList<String> nomes = new ArrayList<String>();
 
@@ -38,5 +47,31 @@ public class exemplos {
         for (int i = 0; i < nomes.size(); i++) {
             System.out.println(i + " = " + nomes.get(i));
         }
+    }
+    private static void linkedListExample(){
+        System.out.println("\nExemplo de LinkedList:");
+
+        //Cria um LinkedList que armazena strings
+        LinkedList<String> nomes = new LinkedList<String>();
+        nomes.add("Arthur");
+        nomes.add("Alice");
+        nomes.add("Henrique");
+
+        for (int i = 0; i < nomes.size(); i++) {
+            System.out.println(nomes.get(i));
+        }
+    }
+    private static void hashMapExample(){
+        System.out.println("\nExemplo de HashMap:");
+        //Cria um hashmap que armazena strings com suas respectivas chaves em string
+        HashMap<String, String> cupons = new HashMap<String, String>();
+
+        //Adiciona as strings ao hashmap com suas respectivas chaves
+        cupons.put("1234","50% de desconto");
+        cupons.put("4321","25% de desconto");
+        
+        //Acessa e imprime no console o conteúdo de 2 elementos do hashmap
+        System.out.println(cupons.get("1234"));
+        System.out.println(cupons.get("4321"));
     }
 }
--- Aula2/README.md
+++ Aula2/README.md
@@ -5,14 +5,21 @@ Vector é um array dinâmico onde sua capacidade e escalonamento podem ser defin
 
 ![exemplo de vector](https://github.com/Buggyes/Algoritmos-e-Estruturas-de-Dados/blob/main/img/Vector.png)
 
-### Arraylist:
+### ArrayList:
 ArrayList é uma forma mais simplificada de um vector, sem a necessidade de manualmente atribuir o seu tamanho inicial e escalonamento, já que na maioria das linguagens isto é feito automáticamente dentro da classe do ArrayList, e também já possui funções utilitarias implementadas como "sort()", e um "foreach()" já integrado dentro de si.
 
-Porém, a maior desvantagem de utilizar um Arraylist é o fato de ser muito custoso em questão de performance, pois ele aloca e desaloca memória toda vez que algo é alterado dentro de si (posição de um elemento, tamanho etc.).
+Porém, a maior desvantagem é a maneira que o ArrayList funciona. Quando o espaço de um LinkedList é preenchido (internamente), ele gera um array novo com um tamanho muito maior, transfere **todos** os elementos do array antigo para o array novo, e deleta o array antigo após isso. Isso é um processo muito custoso, considerando que ArrayLists são utilizados em diversos sistemas e aplicações de grande porte que estão no mercado.
 
 ![exemplo de ArrayList](https://github.com/Buggyes/Algoritmos-e-Estruturas-de-Dados/blob/main/img/ArrayList.png)
 
 ### LinkedList:
+Na prática, um LinkedList é utilizado da mesma forma que um ArrayList, porém a única característica que difere um LinkedList de um ArrayList é a maneira que ele funciona por baixo dos panos.
 
+Um LinkedList armazena seus elementos em "containers". A lista possui um índice do primeiro container, e todo container possui um índice para o próximo container da lista. Ele básicamente funciona que nem uma Queue (fila).
+
+![exemplo de LinkedList](https://github.com/Buggyes/Algoritmos-e-Estruturas-de-Dados/blob/main/img/LinkedList.png)
 
 ### Hashmap:
+HashMaps funcionam essencialmente da mesmo jeito que um ArrayList, a única diferença sendo para criar e acessar os seus elementos, é necessário criar uma chave única para cada elemento.
+
+![exemplo de HashMap](https://github.com/Buggyes/Algoritmos-e-Estruturas-de-Dados/blob/main/img/HashMap.png)
